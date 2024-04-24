d33ec124c35aea9e0dd68880fbeab4ecc057ff03
Soorya D <127182344+Soorya16@users.noreply.github.com>
2024-04-24 23:16:26 +0530

    updated version

--- html-css-js-portfolio-tutorial-2/script.js
+++ html-css-js-portfolio-tutorial-2/script.js
@@ -4,7 +4,6 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-// Add this to your existing JavaScript file
 
 const themeToggleBtn = document.getElementById('theme-toggle-btn');
 const body = document.body;
@@ -13,15 +12,28 @@ function toggleTheme() {
   body.classList.toggle('dark-mode');
   const isDarkMode = body.classList.contains('dark-mode');
   localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+  updateThemeToggleIcon(isDarkMode);
 }
 
-// Check for user's theme preference in local storage
+function updateThemeToggleIcon(isDarkMode) {
+  const moonIcon = document.querySelector('.fa-regular.fa-moon');
+  const sunIcon = document.querySelector('.fa-solid.fa-sun');
+  if (isDarkMode) {
+    moonIcon.style.display = 'none';
+    sunIcon.style.display = 'inline';
+  } else {
+    moonIcon.style.display = 'inline';
+    sunIcon.style.display = 'none';
+  }
+}
+
 const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 const savedTheme = localStorage.getItem('theme');
 
 if (savedTheme) {
   body.classList.toggle(savedTheme);
+  updateThemeToggleIcon(savedTheme === 'dark');
 } else if (userPrefersDark) {
   body.classList.add('dark-mode');
+  updateThemeToggleIcon(true);
 }
-
